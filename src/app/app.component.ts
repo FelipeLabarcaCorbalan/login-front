23c13c9c1e60a45693f3a1c758e3621d4b83c3aa
hramirez289 <hernan.stgohc@gmail.com>
2024-09-03 21:32:23 -0400

    creacion login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
